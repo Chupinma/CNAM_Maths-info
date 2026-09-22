119a8e0b3b2ea07830016ab9919d7fa7705d3296
Maxime CHUPIN <maxime.chupin@outlook.fr>
2026-09-22 18:02:44 +0200

    Trying to commit code changes for the second time

--- Filtre de Carny.py
+++ Filtre de Carny.py
@@ -3,9 +3,14 @@ import numpy as np
 from scipy.signal import convolve2d
 import matplotlib.pyplot as plt
 
-img = Image.open(r"D:\01_Travail_Etudes\01_Ecoles\CNAM\M2_2026-2027\Maths-info\Filtre de Camy\image.jpg").convert("L")
+# Charger l'image en niveaux de gris
+img = Image.open("image.jpg").convert("L")
+
+# Convertir en matrice numpy
 matrice = np.array(img)
 
+# Application du flou gaussien (mode 'same' pour même taille)
+# boundary = "wrap" pour enroulement (bords)
 gaussien = 1/163*np.array([[2,4,6,4,2],
                           [4,9,12,9,4],
                           [6,12,15,12,6],
@@ -13,17 +18,38 @@ gaussien = 1/163*np.array([[2,4,6,4,2],
                           [2,4,6,4,2]])
 img_array_gaussien = convolve2d(matrice, gaussien, mode='same', boundary = 'wrap')
 
-# 4. Afficher l'image originale
+# Définir un noyeau de convolution (ex: détection de contours avec les opérateurs de Sobel)
+kernelx = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]])
+kernely = np.array([[ 1, 2, 1],
+                    [ 0, 0, 0],
+                    [-1,-2,-1]])
+
+# Appliquer les convolutions pour calcul du gradient
+conv1 = convolve2d(matrice, kernelx, mode='same', boundary='fill', fillvalue=0)
+conv2 = convolve2d(matrice, kernely, mode='same', boundary='fill', fillvalue=0)
+
+# Calcul de la norme du gradient
+norm_grad = np.sqrt(np.power(conv1,2)+np.power(conv2,2))
+
+# Afficher l'image originale
 plt.figure(figsize=(18, 10)) #taille de la figure : largeur 18 et hauteur 10
 plt.subplot(2, 1, 1) #2 lignes, 1 colonne, 1er plot
 plt.title("Image originale")
 plt.imshow(matrice, cmap='gray') #cmap = "gray" pour une gamme couleur en niveaux de gris
 plt.axis('off')
 
-# 5. Afficher l'image après le flou gaussien
+# Afficher l'image après le flou gaussien
 plt.subplot(2, 1, 2) #2 lignes, 1 colonne, 2ème plot
 plt.title("Image après flou gaussien")
 plt.imshow(img_array_gaussien, cmap='gray') 
+plt.axis('off')
+
+# Afficher la norme du gradient
+plt.subplot(2, 2, 3) #2 lignes, 2 colonnes, 3ème plot
+plt.title("Norme du Gradient")
+plt.imshow(norm_grad, cmap='gray')
 plt.axis('off')
 
 plt.show()
