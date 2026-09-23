185b9e232f45d8657ecfe31a90d8ea35a8cfb1c8
Maxime CHUPIN <maxime.chupin@outlook.fr>
2026-09-23 21:31:59 +0200

    Fin ex 1 partie 3

--- TD - Filtre de Canny/Exo 1 - etape3.py
+++ TD - Filtre de Canny/Exo 1 - etape3.py
@@ -3,7 +3,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 # Charger l'image
-filepath = r"D:\01_Travail_Etudes\01_Ecoles\CNAM\M2_2026-2027\Maths-info\CNAM_Maths-info\TD - Filtre de Canny\dessin.jpg"
+filepath = r"D:\01_Travail_Etudes\01_Ecoles\CNAM\M2_2026-2027\Maths-info\CNAM_Maths-info\TD - Filtre de Canny\image.jpg"
 img = Image.open(filepath)
 
 # Convertir en matrice numpy
@@ -22,4 +22,17 @@ B = img_array[:, :, 2]
 
 M = 0.299 * R + 0.587 * V + 0.114 * B
 
-print(M)
+# 7. Afficher l'image originale
+plt.figure(figsize=(18, 10))
+plt.subplot(2, 2, 1) #2 lignes, 2 colonnes, 1er plot
+plt.title("Image RGB")
+plt.imshow(img_array, cmap='gray')
+plt.axis('off') # pas d'axes sur le graphique
+
+# 8. Afficher l'image après le flou gaussien
+plt.subplot(2, 2, 2) #2 lignes, 2 colonnes, 2ème plot
+plt.title("Image Grey scale")
+plt.imshow(M, cmap='gray')
+plt.axis('off')
+
+plt.show()
