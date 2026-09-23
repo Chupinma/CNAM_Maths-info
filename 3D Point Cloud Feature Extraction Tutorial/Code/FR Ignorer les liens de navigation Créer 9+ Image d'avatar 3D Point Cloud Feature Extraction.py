d4eac20f357a9010ec2f89a07c7e33633dc69acf
Maxime CHUPIN <maxime.chupin@outlook.fr>
2026-09-23 13:53:54 +0200

    Nouveau projet 3D Point Cloud Feature Extraction sur la chaine de Dr. Florent Poux

---
+++ 3D Point Cloud Feature Extraction Tutorial/Code/FR Ignorer les liens de navigation Créer 9+ Image d'avatar 3D Point Cloud Feature Extraction.py
@@ -0,0 +1,14 @@
+#%% Gathering Data
+
+#%% 1. Python Environnement and Library setup
+
+#Base Libraries
+import numpy as np
+import time
+
+# [Optional] Project Module
+from scipy.spatial import KDTree
+
+# 3D Library
+#import open3d as o3d
+import pyvista as pv
