c03f0dff0d5bb544493121825b7871cc8d0b8c7e
Maxime CHUPIN <maxime.chupin@outlook.fr>
2026-09-23 15:45:12 +0200

    modifications du chemin d'accès du nuage de points

--- 3D Point Cloud Feature Extraction Tutorial/Code/FR Ignorer les liens de navigation Créer 9+ Image d'avatar 3D Point Cloud Feature Extraction.py
+++ 3D Point Cloud Feature Extraction Tutorial/Code/FR Ignorer les liens de navigation Créer 9+ Image d'avatar 3D Point Cloud Feature Extraction.py
@@ -12,3 +12,10 @@ from scipy.spatial import KDTree
 # 3D Library
 #import open3d as o3d
 import pyvista as pv
+
+#%% 2. Load Point Cloud Data (PyVista)
+
+pcd_pv = pv.read(r"...\3D Point Cloud Feature Extraction Tutorial\DATA\MLS_UTWENTE_super_sample.ply")
+
+#Plot quickly with EDL on
+pcd_pv.plot(eye_dome_lighting=True)
